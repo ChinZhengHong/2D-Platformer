519195b9fa37b69afd12e650e92741051e82f945
ChinZhengHong <hong99963@gmail.com>
2025-09-06 21:41:22 +0800

    platform move

--- Script/Platform.gd
+++ Script/Platform.gd
@@ -30,3 +30,44 @@ func _on_timer_timeout():
 func _ready():
 	initial_position = position.y
 	switch_state(State.MOVING_UP)
+
+# change the platforms movement states
+func switch_state(new_state):
+	current_state = new_state
+	match new_state:
+		# if state is moving up, reset progress
+		State.MOVING_UP:
+			progress = 0.0
+			
+		# if state is waiting at top, start the timer to change the state
+		State.WAIT_AT_TOP:
+			$Timer.wait_time = wait_time_at_top # will wait x second before moving
+			$Timer.start()
+			
+		#if state is waiting at bottom, start the timer to change the state
+		State.WAIT_AT_BOTTOM:
+			$Timer.wait_time = wait_time_at_bottom # will wait x second before moving
+			$Timer.start()
+			
+		# if state is moving down, move the platfoprm via the speed and range defined
+		State.MOVING_DOWN:
+			progress = movement_range / movement_speed
+			
+# move our platform
+func _physics_process(delta):
+	match current_state:
+		# if its moving up
+		State.MOVING_UP:
+			progress += delta
+			#change its position
+			position.y = lerp(initial_position, initial_position - movement_range, progress / (movement_range / movement_speed))
+			if progress >= (movement_range / movement_speed):
+				switch_state(State.WAIT_AT_TOP)
+				
+		# if its moving down
+		State.MOVING_DOWN:
+			progress -= delta
+			# change its position	
+			position.y = lerp(initial_position, initial_position - movement_range, progress / (movement_range / movement_speed))
+			if progress <= 0:
+				switch_state(State.WAIT_AT_BOTTOM)

fad230ff748f0eed157269f9335eb33f52b59eb4
ChinZhengHong <hong99963@gmail.com>
2025-11-22 17:34:06 +0800

    add sprite2d and add a script when player touch the collision it will delete the scene

---
+++ Script/pickup.gd
@@ -0,0 +1,18 @@
+### Pickup.gd
+
+extends Area2D
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		get_tree().queue_delete(self)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

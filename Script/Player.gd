8a6952789ae3c4e7a854a436feb81ca4f6ae81d0
ChinZhengHong <hong99963@gmail.com>
2025-12-13 17:17:42 +0800

    .

--- Script/Player.gd
+++ Script/Player.gd
@@ -36,6 +36,7 @@ func _physics_process(delta):
 		player_animation()
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	
 	# countdown for attack boost
 	if Global.is_attacking:
@@ -52,6 +53,8 @@ func _physics_process(delta):
 			#remove bomb
 			if target.name == "Bomb" and Input.is_action_pressed("ui_attack"):
 				Global.is_bomb_moving = false
+=======
+>>>>>>> parent of 649fae7 (fix the bug when player climbing and take the damage , will stuck at ladder)
 =======
 >>>>>>> parent of 649fae7 (fix the bug when player climbing and take the damage , will stuck at ladder)
 =======
